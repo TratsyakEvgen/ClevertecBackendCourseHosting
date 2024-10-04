d68ba28bcd9a6266b88a059b329338bef5dbcf12
TratsyakEvgen <tanchik776@gmail.com>
2024-10-04 09:22:54 +0300

    Initial commit

---
+++ src/test/java/ru/clevertec/hosting/ClevertecBackendCourseHostingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.hosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClevertecBackendCourseHostingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
